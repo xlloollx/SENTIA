017968150292666697a5520b2178802b03151347
xlloollx <xlloollx.gm@gmail.com>
2026-07-09 23:13:51 +0200

    Play = lancer la descente GPS (plus d'enchainement demo), seuil vocal 200 m, sw v9

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    fonctionnement 100% hors connexion sur la rivière.
    Stratégie : network-first pour Sentia.html (mises à jour), cache-first
    pour tout le reste (photos, icônes). Incrémenter VERSION à chaque publication. */
-const VERSION = 'sentia-v8';
+const VERSION = 'sentia-v9';
 
 const ASSETS = [
   './Sentia.html',
