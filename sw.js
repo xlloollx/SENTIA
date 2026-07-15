00378299e28457e1c2968d0aaa70efad2f120dbc
xlloollx <xlloollx.gm@gmail.com>
2026-07-15 23:26:00 +0200

    v1.3 : licence glissante 3 jours, touch-action, sans menu contextuel, sw v14

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    fonctionnement 100% hors connexion sur la rivière.
    Stratégie : network-first pour Sentia.html (mises à jour), cache-first
    pour tout le reste (photos, icônes). Incrémenter VERSION à chaque publication. */
-const VERSION = 'sentia-v13';
+const VERSION = 'sentia-v14';
 
 const ASSETS = [
   './Sentia.html',
